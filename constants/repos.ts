93a22ea0e298a3e02f767d3fcd97242ca0430eba
deepakbhattmits <deepakbhattmits@gmail.com>
2022-04-06 19:35:21 +0530

    Update repos.ts

--- constants/repos.ts
+++ constants/repos.ts
@@ -1,117 +1,117 @@
 /** @format */
 
 export const repos = [
-	{
-		appName: 'Favourite Bar ',
-		repoUrl: 'https:github.com/deepakbhattmits/app-proj-bar-react-javascript',
-		webUrl: 'https:app-proj-bar.herokuapp.com/',
-		description:
-			'This is a app where user can add and remove beers favorite just like adding and remving from wishlist',
-		stacks: [
-			'react js',
-			'redux',
-			'semantic UI',
-			'react-toastify',
-			'node-sass',
-			'redux-persist',
-			'redux-thunk',
-			'node-sass',
-			'enzyme',
-			'typescript'
-		],
-	},
-	{
-		appName: 'Ecommerce App',
-		repoUrl: 'https:github.com/deepakbhattmits/ecom-turing',
-		// webUrl: 'https:ecom-turing.herokuapp.com/',
-		description:
-			'This demo app ecommerce app consuming api to render categories, products and color swatches',
-		stacks: [
-			'react js',
-			'redux',
-			'semantic UI',
-			'axios',
-			'node-sass',
-			'react-alice-carousel',
-			'redux-persist',
-			'redux-thunk',
-		],
-	},
-	{
-		appName: 'Next cookie authentication',
-		repoUrl: 'https:github.com/deepakbhattmits/next-cookie-auth',
-		webUrl: 'https:next-cookie-auth.deepakbhattmits.vercel.app',
-		description:
-			'This app is built in nextjs, cookie based authentication using',
-		stacks: [
-			'nextjs',
-			'react',
-			'react-dom',
-			'react-hook-form',
-			'cookie-parser',
-			'axios',
-		],
-	},
-	{
-		appName: 'App Places',
-		repoUrl: 'https:github.com/deepakbhattmits/app-places',
-		webUrl: 'https:app-places-2d069.web.app/',
-		description:
-			'In this user can add, update and delete places and for storing data used mongoDB database',
-		stacks: ['react js', 'redux', 'mongodb', 'node js', 'mongoose', 'mapboxgl'],
-	},
-	{
-		appName: 'App Quiz',
-		repoUrl: 'https:github.com/deepakbhattmits/app-quiz',
-		webUrl: 'https:app-quiz-react.herokuapp.com',
-		description:
-			'This is a demo app to demonstrate QUIZ app developed in react and used redux, redux-thunk for state management ',
-		stacks: [
-			'react js',
-			'react-dom',
-			'redux',
-			'redux-thunk',
-			'react-bootstrap',
-			'node-sass',
-			'bootstarp',
-			'google fonts',
-		],
-	},
-	{
-		appName: 'Hacker Next',
-		repoUrl: 'https:github.com/deepakbhattmits/hacker-next',
-		webUrl: ' https:hacker-next.deepakbhattmits.vercel.app',
-		description:
-			"it's a clone of Hacker news website https:news.ycombinator.com some functionality is in progress",
-		stacks: ['next js', 'react', 'react-dom', 'isomorphic-unfetch'],
-	},
-	{
-		appName: 'node js shop',
-		repoUrl: 'https:github.com/deepakbhattmits/app-node',
-		webUrl: 'https:app-node-home.herokuapp.com/',
-		description:
-			'a shop build in node js and use view engine EJS using database mongoDB',
-		stacks: [
-			'react js',
-			'redux',
-			'semantic UI',
-			'mongoDB',
-			'view engine(EJS)',
-			'nodejs',
-			'express',
-		],
-	},
-	// {
-	// 	appName: 'Angular app',
-	// 	repoUrl: 'https:github.com/deepakbhattmits/angularnew',
-	// 	webUrl: 'http://omangular.epizy.com/',
-	// 	description:
-	// 		'This is a demo app developed in angular js version 1 and database used mySQL',
-	// 	stacks: [
-	// 		'angular js',
-	// 		'mySQL',
-	// 		'PHP Version: 5.6.28',
-	// 		'Server version: 10.1.19-MariaDB',
-	// 	],
-	// },
+  {
+    appName: "Favourite Bar ",
+    repoUrl: "https:github.com/deepakbhattmits/app-proj-bar-react-javascript",
+    webUrl: "https:app-proj-bar.herokuapp.com/",
+    description:
+      "This is a app where user can add and remove beers favorite just like adding and remving from wishlist",
+    stacks: [
+      "react js",
+      "redux",
+      "semantic UI",
+      "react-toastify",
+      "node-sass",
+      "redux-persist",
+      "redux-thunk",
+      "node-sass",
+      "enzyme",
+      "typescript",
+    ],
+  },
+  {
+    appName: "Ecommerce App",
+    repoUrl: "https:github.com/deepakbhattmits/ecom-turing",
+    webUrl: "https:ecom-turing.herokuapp.com/",
+    description:
+      "This demo app ecommerce app consuming api to render categories, products and color swatches",
+    stacks: [
+      "react js",
+      "redux",
+      "semantic UI",
+      "axios",
+      "node-sass",
+      "react-alice-carousel",
+      "redux-persist",
+      "redux-thunk",
+    ],
+  },
+  {
+    appName: "Next cookie authentication",
+    repoUrl: "https:github.com/deepakbhattmits/next-cookie-auth",
+    webUrl: "https:next-cookie-auth.deepakbhattmits.vercel.app",
+    description:
+      "This app is built in nextjs, cookie based authentication using",
+    stacks: [
+      "nextjs",
+      "react",
+      "react-dom",
+      "react-hook-form",
+      "cookie-parser",
+      "axios",
+    ],
+  },
+  {
+    appName: "App Places",
+    repoUrl: "https:github.com/deepakbhattmits/app-places",
+    webUrl: "https:app-places-2d069.web.app/",
+    description:
+      "In this user can add, update and delete places and for storing data used mongoDB database",
+    stacks: ["react js", "redux", "mongodb", "node js", "mongoose", "mapboxgl"],
+  },
+  {
+    appName: "App Quiz",
+    repoUrl: "https:github.com/deepakbhattmits/app-quiz",
+    webUrl: "https:app-quiz-react.herokuapp.com",
+    description:
+      "This is a demo app to demonstrate QUIZ app developed in react and used redux, redux-thunk for state management ",
+    stacks: [
+      "react js",
+      "react-dom",
+      "redux",
+      "redux-thunk",
+      "react-bootstrap",
+      "node-sass",
+      "bootstarp",
+      "google fonts",
+    ],
+  },
+  {
+    appName: "Hacker Next",
+    repoUrl: "https:github.com/deepakbhattmits/hacker-next",
+    webUrl: " https:hacker-next.deepakbhattmits.vercel.app",
+    description:
+      "it's a clone of Hacker news website https:news.ycombinator.com some functionality is in progress",
+    stacks: ["next js", "react", "react-dom", "isomorphic-unfetch"],
+  },
+  {
+    appName: "node js shop",
+    repoUrl: "https:github.com/deepakbhattmits/app-node",
+    webUrl: "https:app-node-home.herokuapp.com/",
+    description:
+      "a shop build in node js and use view engine EJS using database mongoDB",
+    stacks: [
+      "react js",
+      "redux",
+      "semantic UI",
+      "mongoDB",
+      "view engine(EJS)",
+      "nodejs",
+      "express",
+    ],
+  },
+  // {
+  // 	appName: 'Angular app',
+  // 	repoUrl: 'https:github.com/deepakbhattmits/angularnew',
+  // 	webUrl: 'http://omangular.epizy.com/',
+  // 	description:
+  // 		'This is a demo app developed in angular js version 1 and database used mySQL',
+  // 	stacks: [
+  // 		'angular js',
+  // 		'mySQL',
+  // 		'PHP Version: 5.6.28',
+  // 		'Server version: 10.1.19-MariaDB',
+  // 	],
+  // },
 ];
