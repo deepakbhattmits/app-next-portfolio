8e4d795f1366223a15e9fad539a0c76b0bc050d7
Deepak Bhatt <deepakbhattmits@gmail.com>
2025-10-04 22:30:05 +0530

    New changes

---
+++ lib/init-middleware.ts
@@ -0,0 +1,12 @@
+// lib/init-middleware.js
+export default function initMiddleware(middleware) {
+  return (req, res) =>
+    new Promise((resolve, reject) => {
+      middleware(req, res, (result) => {
+        if (result instanceof Error) {
+          return reject(result);
+        }
+        return resolve(result);
+      });
+    });
+}
--- pages/api/v1/[questionId].ts
+++ pages/api/v1/[questionId].ts
@@ -1,8 +1,19 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { connectToDatabase } from "../../../helpers";
+import Cors from 'cors';
+import initMiddleware from '../../../lib/init-middleware'; // adjust path accordingly
+
+const cors = initMiddleware(
+  Cors({
+    methods: ['GET'],
+    origin: 'https://app-next-portfolio.vercel.app', // ← set your frontend URL here
+    credentials: true,
+  })
+);
 
 const getAnswer = async (req: NextApiRequest, res: NextApiResponse) => {
-  const { questionId } = req?.query;
+   await cors(req, res); // 👈 Important: run the CORS middleware first
+   // const { questionId } = req?.query;
   const client = await connectToDatabase();
   const db = client?.db();
   const answer = await db?.collection("answers").findOne({ id: +questionId });
