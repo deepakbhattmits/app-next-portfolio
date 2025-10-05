9086e37e6d1f36b3f703bbd55ec9d1ec820580e0
Deepak Bhatt <deepakbhattmits@gmail.com>
2025-10-05 20:30:17 +0530

    New changes evening 5thOct-final

--- pages/api/v1/index.ts
+++ pages/api/v1/index.ts
@@ -1,7 +1,8 @@
-export default async (req, res) => {
+const indexFunc = async (req, res) => {
   const { method } = req;
   // This will allow OPTIONS request
   if (method === "OPTIONS") {
     return res.status(200).send("ok");
   }
 };
+export default indexFunc
