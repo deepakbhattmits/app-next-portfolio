a9a88814482d94ee78de959a5bdbc28caf4d7159
Deepak Bhatt <deepakbhattmits@gmail.com>
2025-10-05 19:53:56 +0530

    New changes evening 5thOct

--- utils/api.ts
+++ utils/api.ts
@@ -9,7 +9,7 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: !!process.env.NODE_ENV?.match(/development/i)
     ? "http://localhost:3000"
-    : "https://app-next-portfolio-deepakbhattmits.vercel.app",
+    : "https://app-next-portfolio.vercel.app",
   headers: {
     "Content-Type": "application/json",
   },
