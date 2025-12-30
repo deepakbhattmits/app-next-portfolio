62bebf8366eeb10df465ba6ad61c192b436c4b6c
Deepak Bhatt <deepakbhattmits@gmail.com>
2025-12-30 21:36:05 +0530

    updated config

--- next.config.js
+++ next.config.js
@@ -22,7 +22,11 @@ const nextConfig = {
         // matching all API routes
         source: "/api/:path*",
         headers: [
-          { key: "Access-Control-Allow-Credentials", value: "*" },
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://app-next-portfolio.vercel.app",
+          },
           {
             key: "Access-Control-Allow-Methods",
             value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
@@ -31,8 +35,7 @@ const nextConfig = {
             key: "Access-Control-Allow-Headers",
             value:
               "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-          { key: 'Access-Control-Allow-Origin', value: 'https://app-next-portfolio.vercel.app' }
+          }
         ],
       },
     ];
