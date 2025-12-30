9e0c583a6a3111d359c12c1578382a59a508254b
Deepak Bhatt <deepakbhattmits@gmail.com>
2025-12-30 19:25:18 +0530

    Reverted

--- pages/api/v1/post-question.ts
+++ pages/api/v1/post-question.ts
@@ -1,24 +1,26 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { connectToDatabase } from "../../../helpers";
+import { randomUUID } from 'crypto';
 
-export const fetchQuestions = async () => {
-  const client = await connectToDatabase();
-  const db = client?.db();
-  const questions = await db?.collection("questions").find().toArray();
-  client.close();
-  return questions;
-};
-export const fetchAnswers = async () => {
-  const client = await connectToDatabase();
+// export const fetchQuestions = async () => {
+//   const client = await connectToDatabase();
+//   const db = client?.db();
+//   const questions = await db?.collection("questions").find().toArray();
+//   client.close();
+//   return questions;
+// };
+// export const fetchAnswers = async () => {
+//   const client = await connectToDatabase();
 
-  const db = client?.db();
-  const answers = await db?.collection("answers").find().toArray();
-  client.close();
-  return answers;
-};
+//   const db = client?.db();
+//   const answers = await db?.collection("answers").find().toArray();
+//   client.close();
+//   return answers;
+// };
 const postQuestion = async (req: NextApiRequest, res: NextApiResponse) => {
+
   if (!!req?.method?.match(/post/i)) {
-    const questions = await fetchQuestions();
+    const uuid = randomUUID()
     const {
       values: {
         userId,
@@ -32,15 +34,14 @@ const postQuestion = async (req: NextApiRequest, res: NextApiResponse) => {
     const client = await connectToDatabase();
 
     const db = client.db();
-    let lastElement: any = questions.slice(-1);
     try {
       await db.collection("questions").insertOne({
-        id: +lastElement[0]?.id + 1,
+        id: uuid,
         que,
         userId,
       });
       await db.collection("answers").insertOne({
-        id: +lastElement[0]?.id + 1,
+        id: uuid,
         answer,
       });
     } catch (err) {
