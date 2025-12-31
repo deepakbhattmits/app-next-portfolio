ba685df79715021623c9f2c15c59aaf69406a9fc
Deepak Bhatt <deepakbhattmits@gmail.com>
2025-12-31 12:14:16 +0530

    removed commented lines

--- utils/api.ts
+++ utils/api.ts
@@ -1,10 +1,4 @@
 import axios from "axios";
-// export const api = axios.create({
-//   baseURL: "http://localhost:3000",
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-// });
 
 export const api = axios.create({
   baseURL: !!process.env.NODE_ENV?.match(/development/i)
