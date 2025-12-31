8fd95b7fefdf1ed265998d3c30e5030ab4817f1d
Deepak Bhatt <deepakbhattmits@gmail.com>
2025-12-31 12:10:55 +0530

    updated config

--- pages/api/v1/index.ts
+++ pages/api/v1/index.ts
@@ -1,12 +1,5 @@
 export default async (req, res) => {
   const { method } = req;
-  // Allow requests from your specific origin
-  res.setHeader('Access-Control-Allow-Origin', 'https://app-next-portfolio.vercel.app');
-  // Specify allowed HTTP methods
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  // Specify allowed headers
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
   // This will allow OPTIONS request
   if (method === "OPTIONS") {
     return res.status(200).send("ok");
